33166e455ecec269997ee35c36ffde7ea5e07af7
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-12-23 04:07:52 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,34 @@
+{
+  "name": "Bons plans entre mjeziya ",
+  "short_name": "Tnwork",
+  "start_url": "index2.html",
+   "scope": "/",
+  "display": "standalone", 
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "image/1.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "image/1.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "id": "Tnworkh1m",
+  "iarc_rating_id": "Tnworkh1m",
+  "prefer_related_applications": true,
+  "shortcuts": [
+    {
+      "name": "Tnwork",
+      "url": "",
+      "description": ""
+    }
+  ],
+  "categories": [
+    "business"
+  ]
+}
