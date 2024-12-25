def0662017b77fa84c904fd14637673b3eaf5be3
CME2024 <142491184+Oussamahammami2023@users.noreply.github.com>
2024-12-26 00:30:14 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'offline-cache-v3'; // قم بتغيير هذا الرقم في كل تحديث
+const CACHE_NAME = 'boutique1-v3'; // قم بتغيير هذا الرقم في كل تحديث
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
